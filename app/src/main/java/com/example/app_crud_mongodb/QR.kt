5703266bf771ef69c20d73d88a11eefb93efb0e3
smz7z <santimgb98@gmail.com>
2025-10-11 01:01:15 +0200

    Modificación interfaz

--- app/src/main/java/com/example/app_crud_mongodb/QR.kt
+++ app/src/main/java/com/example/app_crud_mongodb/QR.kt
@@ -1,5 +1,6 @@
 package com.example.app_crud_mongodb
 
+
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
@@ -9,36 +10,25 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class SegundoActivity  : AppCompatActivity() {
+class QR  : AppCompatActivity() {
     @SuppressLint("MissingInflatedId", "MissingSuperCall")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_2)
+        setContentView(R.layout.qr)
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.segundo)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.QR)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        fun salirApp() {
-            finishAffinity()
-        }
 
-        val acceder = findViewById<Button>(R.id.acceder)
-        val zona_cliente = findViewById<Button>(R.id.zona_cliente)
-        val salir = findViewById<Button>(R.id.salir)
+        val volver = findViewById<Button>(R.id.volver)
 
-        acceder.setOnClickListener {
-            val intent = Intent(this, QR::class.java)
+        volver.setOnClickListener {
+            val intent = Intent(this, SegundoActivity::class.java)
             startActivity(intent)
         }
-        zona_cliente.setOnClickListener {
-            val intent = Intent(this, TercerActivity::class.java)
-            startActivity(intent)
-        }
-        salir.setOnClickListener {
-            salirApp()
-        }
+
     }
 }
--- app/src/main/java/com/example/app_crud_mongodb/TercerActivity.kt
+++ app/src/main/java/com/example/app_crud_mongodb/TercerActivity.kt
@@ -1,6 +1,8 @@
 package com.example.app_crud_mongodb
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,13 @@ class TercerActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val volver = findViewById<Button>(R.id.volver)
+
+        volver.setOnClickListener {
+            val intent = Intent(this, SegundoActivity::class.java)
+            startActivity(intent)
         }
     }
 }
